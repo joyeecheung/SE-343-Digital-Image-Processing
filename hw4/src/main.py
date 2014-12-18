5cf46874c9ce122aaaad0f17476692583676f365
Joyee Cheung <joyeec9h3@gmail.com>
2014-12-18 12:30:44 +0800

    guass and sap denoising

--- hw4/src/main.py
+++ hw4/src/main.py
@@ -8,6 +8,7 @@ import numpy as np
 from PIL import Image
 
 from util import arithmetic_mean, harmonic_mean, c_harmonic_mean
+from util import median_filter, max_filter, min_filter
 from noise import gauss_noise, sap_noise
 
 
@@ -35,7 +36,7 @@ def test_filter(filename, result_dir):
         savewith(result, 'c-harmonic-mean-%d-%d.png' % size)
 
 
-def test_noise(filename, result_dir):
+def test_gauss(filename, result_dir):
     im = Image.open(filename)
 
     def savewith(result, name):
@@ -43,17 +44,84 @@ def test_noise(filename, result_dir):
         result.save(result_path)
         print '[Saved] ' + result_path
 
+    # generate noise
     mean, var = 0, 40
-    result = gauss_noise(im, mean, var)
-    savewith(result, 'gauss-%d-%d.png' % (mean, var))
+    noisy = gauss_noise(im, mean, var)
+    savewith(noisy, 'gauss-%d-%d.png' % (mean, var))
+
+    # arithmetic mean filtering
+    result = arithmetic_mean(noisy, (3, 3))
+    savewith(result, 'gauss-arithmetic.png')
+
+    # TODO: geometric mean filtering
+
+    # harmonic mean filtering
+    result = harmonic_mean(noisy, (3, 3))
+    savewith(result, 'gauss-harmonic.png')
+
+    # contraharmonic mean filtering
+    result = c_harmonic_mean(noisy, (3, 3), -1.5)
+    savewith(result, 'gauss-contraharmonic.png')
+
+    # median filtering
+    result = median_filter(noisy, (3, 3))
+    savewith(result, 'gauss-median.png')
+
+
+def test_salt(filename, result_dir):
+    im = Image.open(filename)
+
+    def savewith(result, name):
+        result_path = os.path.join(result_dir, name)
+        result.save(result_path)
+        print '[Saved] ' + result_path
 
     ps, level = 0.2, 256
-    result = sap_noise(im, level, ps=ps)
-    savewith(result, 'salt-%d.png' % (int(100 * ps)))
+    noisy = sap_noise(im, level, ps=ps)
+    savewith(noisy, 'salt-%d.png' % (int(100 * ps)))
+
+    q_neg, q_pos = 1.5, -1.5
+    result = c_harmonic_mean(noisy, (3, 3), q_neg)
+    savewith(result, 'salt-contraharmonic-%s.png' % (str(q_neg)))
+    result = c_harmonic_mean(noisy, (3, 3), q_pos)
+    savewith(result, 'salt-contraharmonic-%s.png' % (str(q_pos)))
+
+
+def test_sap(filename, result_dir):
+    im = Image.open(filename)
+
+    def savewith(result, name):
+        result_path = os.path.join(result_dir, name)
+        result.save(result_path)
+        print '[Saved] ' + result_path
 
     psap, level = 0.2, 256
-    result = sap_noise(im, level, psap=psap)
-    savewith(result, 'salt-and-pepper-%d.png' % (int(100 * psap)))
+    noisy = sap_noise(im, level, psap=psap)
+    savewith(noisy, 'sap-%d.png' % (int(100 * psap)))
+
+    # arithmetic mean filtering
+    result = arithmetic_mean(noisy, (3, 3))
+    savewith(result, 'sap-arithmetic.png')
+
+    # harmonic mean filtering
+    result = harmonic_mean(noisy, (3, 3))
+    savewith(result, 'sap-harmonic.png')
+
+    # contraharmonic mean filtering
+    result = c_harmonic_mean(noisy, (3, 3), -1.5)
+    savewith(result, 'sap-contraharmonic.png')
+
+    # max filtering
+    result = max_filter(noisy, (3, 3))
+    savewith(result, 'sap-max.png')
+
+    # min filtering
+    result = min_filter(noisy, (3, 3))
+    savewith(result, 'sap-min.png')
+
+    # median filtering
+    result = median_filter(noisy, (3, 3))
+    savewith(result, 'sap-median.png')
 
 
 def main():
@@ -71,8 +139,13 @@ def main():
         print "Result directory does not exist, created."
         os.makedirs(result_dir)
 
-    # test_filter(os.path.join(source_path, 'task_1.png'), result_dir)
-    test_noise(os.path.join(source_path, 'task_2.png'), result_dir)
+    task_1_path = os.path.join(source_path, 'task_1.png')
+    task_2_path = os.path.join(source_path, 'task_2.png')
+
+    #test_filter(task_1_path, result_dir)
+    #test_gauss(task_2_path, result_dir)
+    #test_salt(task_2_path, result_dir)
+    test_sap(task_2_path, result_dir)
 
 if __name__ == "__main__":
     main()
